c0057bebd2f00aafdfdb788411f1a45cda7b2f44
W9005783 <W9005783@adc.com>
2021-04-01 11:47:07 +0800

    添加移动端侧边栏

--- src/views/index.vue
+++ src/views/index.vue
@@ -12,6 +12,9 @@
           </div>
           <span class="r_img"><img src="//12452007.s61i.faiusr.com/4/AD0Ip4H4BRAEGAAgvrWYwwUovtWd1AIwNDg4.png"
                  alt=""></span>
+          <span @click='show=true'
+                class="mian_img"><img src="../../static/common/icon/mianbaoxie.png"
+                 alt=""></span>
         </div>
       </div>
     </div>
@@ -31,6 +34,26 @@
     <footer>
       <fooBar />
     </footer>
+    <p v-show="arrowShow"
+       @click="scollTop"
+       class="fixedImg"><img :src="require('../../static/common/icon/toUp.png')"
+           alt="">
+    </p>
+    <!-- 移动端右侧栏 -->
+    <div :class="{hide_bar:!show}"
+         class="right_bar"
+         @click="hideBar($event)">
+      <ul>
+        <li :class="{active: i===index,bar_li:true}"
+            @click="index=i"
+            v-for="(item,i) in tabData"
+            :key="item.key">
+          <router-link :to="item.url">
+            {{ item.title }}
+          </router-link>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -40,8 +63,46 @@ export default {
   components: {
     fooBar
   },
+  mounted() {
+    window.addEventListener('scroll', this.handleScroll, true)
+    this.calcWidth()
+  },
+  methods: {
+    hideBar(e) {
+      if (e.target.offsetParent && e.target.offsetParent.classList.value === 'active bar_li' || e.target.classList.value === 'right_bar') {
+        this.show = false
+      } else {
+        this.show = true
+      }
+    },
+
+    calcWidth() {
+      let sceenW = window.innerWidth
+      if (sceenW > 700) {
+        this.phonWay = false
+      } else {
+        this.phonWay = true
+      }
+    },
+    scollTop() {
+      window.scroll(0, 0);
+    },
+    handleScroll(e) {
+      let scrollTop = e.target.documentElement.scrollTop || e.target.body.scrollTop;      // 执行代码
+      if (scrollTop > 200) {
+        this.arrowShow = true
+      } else {
+        this.arrowShow = false
+      }
+    },
+  }
+  ,
   data() {
     return {
+      show: false,
+      index: 0,
+      phonWay: false,
+      arrowShow: false,
       tabData: [
         {
           title: '首页',
@@ -127,6 +188,56 @@ export default {
   width: 100%;
   height: 100%;
 }
+.right_bar {
+  position: fixed;
+  z-index: 99999;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  background-color: rgba(0, 0, 0, 0.3);
+  transition: all linear 0.4s;
+  ul {
+    transition: all linear 2s;
+    position: fixed;
+    z-index: 9999;
+    right: 0;
+    height: 100%;
+    width: 70%;
+    background-color: rgb(255, 255, 255);
+    li {
+      padding: 0 10px;
+      font-size: 8px;
+      height: 36px;
+      line-height: 36px;
+      border-bottom: 1px solid #f0f0f0;
+      position: relative;
+      a {
+        position: absolute;
+        top: 0;
+        left: 10px;
+        height: 100%;
+        width: 100%;
+        color: #333333;
+        &::before {
+          display: none;
+        }
+      }
+      &.active {
+        background-color: #1890ff;
+        a {
+          color: #fff;
+        }
+      }
+    }
+  }
+}
+.hide_bar {
+  display: none;
+  ul {
+    right: -70%;
+  }
+}
 </style>
 <style>
 .router-link-exact-active {
@@ -143,5 +254,23 @@ export default {
   width: 50%;
   height: 1px;
   background-color: #40a9ff;
+}
+.fixedImg {
+  position: fixed;
+  z-index: 999;
+  right: 30px;
+  bottom: 30px;
+  width: 26px;
+  height: 26px;
+  border-radius: 99px;
+  box-shadow: 2px 2px 2px #888888;
+  background-color: #fff;
+  opacity: 0.8;
+}
+.mian_img {
+  right: 5px;
+  width: 26px;
+  margin-left: 5px;
+  display: none;
 }
 </style>
